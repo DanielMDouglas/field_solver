65ffac9edee0e40643737bd2cd2cefe522a97214
Daniel Douglas <dougl215@msu.edu>
2019-03-11 13:27:19 -0700

    added edep

--- src/drift.cxx
+++ src/drift.cxx
@@ -8,8 +8,9 @@
 
 const double q = -1.602e-19; // C
 const double m = 9.109e-31; // kg
-const double mu = 1.e4; // cm^2 V^-1 s^-1
-const double dt = 1.e-11; // s
+const double mu = 1.e6; // cm^2 V^-1 s^-1
+const double dt = 1.e-12; // s
+// const double vDrift = 1.e6; // cm/s
 
 const int max_iter = 1e4;
 const double ds = 0.01; // differentiation distance
@@ -23,7 +24,8 @@ std::vector <double> Fe(std::vector <double> pos, scalarField V)
   return std::vector <double> {1.e7*q*Ex, 1.e7*q*Ey, 1.e7*q*Ez};
 }
 
-std::vector <double> Fd(std::vector <double> mom) {
+std::vector <double> Fd(std::vector <double> mom)
+{
   return (1.e4*q/(m*mu))*mom;
 }
 
@@ -34,28 +36,33 @@ path drift_path(std::vector <double> init_pos, scalarField V, boundary b)
   std::vector <double> pos = init_pos;
   std::vector <double> mom = {0, 0, 0};
   
-  // std::cout << "Initial pos: " << "(" << pos[0] << ", " << pos[1] << ", " << pos[2] << ")" << std::endl;
+  std::cout << "Initial pos: " << "(" << pos[0] << ", " << pos[1] << ", " << pos[2] << ")" << std::endl;
   
   while ( true ) {
     trajectory.steps.push_back(pos);
-    if ( (pos[0] < -0.95) or (pos[0] > 0.95) or (pos[2] < -0.15) ) {
-      // std::cout << "particle left boundary!" << std::endl;
+    if ( (pos[0] < b.Xmin) or (pos[0] > b.Xmax) or
+	 (pos[1] < b.Ymin) or (pos[1] > b.Ymax) or
+	 (pos[2] < b.Zmin) or (pos[2] > b.Zmax) ) {
+      std::cout << "particle left boundary!" << std::endl;
       trajectory.fate = "OOB";
       break;
     }
     else if ( t >= dt*max_iter ) {
-      // std::cout << "particle timed out!" << std::endl;
+      std::cout << "particle timed out!" << std::endl;
       trajectory.fate = "OOT";
       break;
     }
     else if ( b.is_in_boundary(pos[0], pos[1], pos[2]) ) {
-      // std::cout << "particle terminated in a volume!" << std::endl;
+      std::cout << "particle terminated in a volume!" << std::endl;
       trajectory.fate = "volume";
       break;
     }
     else {
-      std::vector <double> Fnet = Fe(pos, V) + Fd(mom);
-      mom = mom + Fnet*dt;
+      // std::vector <double> Fnet = Fe(pos, V) + Fd(mom);
+      // mom = mom + Fnet*dt;
+      mom = mom + Fd(mom)*dt;
+      mom = mom + Fe(pos, V)*dt;
+      // mom = mom*(m*vDrift/mag(mom));
       pos = pos + mom*(dt/m);
       t += dt;
     }
@@ -63,16 +70,17 @@ path drift_path(std::vector <double> init_pos, scalarField V, boundary b)
   
   trajectory.arrivalTime = t;
 
-  // std::cout << "Final pos: " << "(" << pos[0] << ", " << pos[1] << ", " << pos[2] << ")" << std::endl;
-  // std::cout << "Arrival time: " << t << std::endl;
-  // std::cout << std::endl;
+  std::cout << "Final pos: " << "(" << pos[0] << ", " << pos[1] << ", " << pos[2] << ")" << std::endl;
+  std::cout << "Arrival time: " << t << std::endl;
+  std::cout << "Fate: " << trajectory.fate << std::endl;
+  std::cout << std::endl;
 
   return trajectory;
 }
 
 void drift_and_save(double xi, scalarField potential, boundary detector)
 {
-  path trajectory = drift_path(std::vector <double> {xi, 0.15, 1.5}, potential, detector);
+  path trajectory = drift_path(std::vector <double> {xi, 0.15, 0.8}, potential, detector);
   std::ostringstream stringStream;
   stringStream << "paths/drift_from_" << xi << ".dat";
   trajectory.print_to_file(stringStream.str());
@@ -82,8 +90,9 @@ int main()
 {
   scalarField potential = scalarField("wire_cathode_300V.dat");
   boundary detector;
-
-  int nPaths = 50;
+  detector.Zmin = -0.2;
+  
+  const int nPaths = 50;
 
   // std::vector <std::thread> threads {nPaths};
   std::thread * threads [nPaths];
